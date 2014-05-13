d619b25fc1c8bd019fd5d2bd2cb9cbdb3b579fa1
paulrhodes <paul.rhodes@digital.justice.gov.uk>
2014-05-13 12:50:45 +0100

    Typo in config.js

--- metrics/templates/grafana/config.js
+++ metrics/templates/grafana/config.js
@@ -1,4 +1,4 @@
-** @scratch /configuration/config.js/1
+/** @scratch /configuration/config.js/1
  * == Configuration
  * config.js is where you will find the core Grafana configuration. This file contains parameter that
  * must be set before Grafana is run for the first time.
